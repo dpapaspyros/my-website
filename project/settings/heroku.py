87599b6b6147b3c6dd27a5ef6cd5e9bdefe99c49
Dimitris Papaspyros <dipapaspyros@gmail.com>
2021-01-02 15:42:47 +0200

    Remove https redirect

--- project/settings/heroku.py
+++ project/settings/heroku.py
@@ -12,8 +12,3 @@ ALLOWED_HOSTS = ['*']
 
 # Whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# SSl settings
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
